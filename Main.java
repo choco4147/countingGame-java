d7c703101f9d52d7bf96b1b17a9038cc1b98f7c6
choco4147 <100542480+choco4147@users.noreply.github.com>
2022-09-14 12:00:00 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
         String newline = System.getProperty("line.separator");
         //explain the game
         System.out.println("This is a counting game where you can choose the number that you are counting to, as well as the interval that you will count at.");
+        System.out.println("the goal is to be the player who says the target word, and both players will take turns counting up.");
         System.out.println("This program will say how many moves it will take to win, as well as who should go first to win.");
         //ask user for target number
         System.out.print("The number we will count to:");
@@ -16,15 +17,31 @@ public class Main {
         int interval = scan.nextInt();
         System.out.print(newline);
         //determine who should go first
-        int startNum = interval % target;
+        int startNum = target;
+        while(startNum > interval) {
+            startNum = startNum - (1 + interval);
+        }
         if(startNum <= interval) {
             System.out.println("The computer will go first to ensure that it will win.");
         }
         else {
             System.out.println("The computer will go second to ensure that it will win.");
         }
-        //determine number of moves to win
-        int moves = Math.round(target / interval + 1 );
+        //print the numbers you need to say to win
+        int moves = 0;
+        int number = startNum;
+        System.out.print("It will say the following numbers: ");
+        while(target >= number) {
+            System.out.print(number + " ");
+            number = number + interval + 1;
+            moves = moves + 2;
+        }
+        //print number of moves to win
+        System.out.print(newline);
+        if(startNum <= interval) {
+            moves = moves - 1;
+        }
         System.out.println("It will take at least " + moves + " moves to win");
+        
     }
 }
